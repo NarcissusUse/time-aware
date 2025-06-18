5456323b32a6d48af3d0ada509c83ad3ba341752
NarcissusUse <159668717+NarcissusUse@users.noreply.github.com>
2025-06-18 18:43:55 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # Code based on https://github.com/ghdtjr/A-LLMRec
-
 import os
 import sys
 import argparse
-
+import importlib
 from utils import *
 from train_model import *
 
@@ -19,26 +18,28 @@ if __name__ == "__main__":
     parser.add_argument("--train", action='store_true')
     parser.add_argument("--extract", action='store_true')
     parser.add_argument("--token", action='store_true')
-
     parser.add_argument("--save_dir", type=str, default='seqllm')
-
     parser.add_argument('--batch_size', default=4, type=int)
     parser.add_argument('--batch_size_infer', default=4, type=int)
     
-    parser.add_argument('--infer_epoch', default=1, type=int)
+    # Add model version argument
+    parser.add_argument("--model_version", type=str, default='', 
+                       help='Model version suffix (B, C, D, E, F). Leave empty for base model')
     
-    parser.add_argument('--maxlen', default=128, type=int)#50
+    parser.add_argument('--infer_epoch', default=1, type=int)
+    parser.add_argument('--maxlen', default=128, type=int)
     parser.add_argument('--num_epochs', default=10, type=int)
     parser.add_argument("--stage2_lr", type=float, default=0.0001)
     parser.add_argument('--nn_parameter', default=False, action='store_true')
     
-
     args = parser.parse_args()
     
-    if args.device =='hpu':
+    # Set device
+    if args.device == 'hpu':
         args.device = torch.device('hpu')
     else:
         args.device = 'cuda:' + str(args.device)
     
+    # Pass the model version to the training function
     if args.train:
         train_model(args)
